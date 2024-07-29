074caf66c77405f42cbd8e3aa1707d180331972c
Margaux Imbert <margaux69.imbert@gmail.com>
2024-07-29 17:43:29 +0200

    minor debug

--- script_auto_sarek.sh
+++ script_auto_sarek.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-set -e
+#TODO: ajouter log
+#TODO: write readme
 
 # load variables
 source auto_sarek.conf  #TODO: modify path
 source $(conda info --base)/etc/profile.d/conda.sh
 
+CWD="$(pwd)/"
 HEADER="[HEADER]
 A list of all the runs in this directory and their status regarding the sarek workflow
     0: to ignore
@@ -21,7 +23,7 @@ A list of all the runs in this directory and their status regarding the sarek wo
 make_runs_file () {
     # argument 1 runs ouput directory
     # argument 2 run list
-    echo "${HEADER}" >"$1${RUNS_FILE}"
+    echo "${HEADER}" >"$1${RUNS_FILE}" #TODO: issues ?
     for RUN in $2
         do
         echo "${RUN} 0" >>"$1${RUNS_FILE}"
@@ -44,10 +46,11 @@ get_bed_file () {
             LINE=$(grep "${RUN}" "${SEQ_PATH}${RUNS_FILE}")
         else
             local tmp=${DESCRIPTION#*,}
-            BEDFILE=${tmp%#*}
+            BEDNAME=${tmp%.bed#*}
         fi
     else
         change_status 0
+        LINE=$(grep "${RUN}" "${SEQ_PATH}${RUNS_FILE}")
     fi
 }
 
@@ -56,7 +59,7 @@ create_samplesheet () {
     echo "patient,sample,lane,fastq_1,fastq_2" >"${SEQ_PATH}${RUN}/${SAMPLESHEET_SAREK}"
     # fastq path in the run directory
     case "${SEQ_PATH}" in
-        "${MINISEQ_PATH}") local FASTQ_PATH="Alignment_*/${RUN}*/Fastq/" ;;
+        "${MINISEQ_PATH}") local FASTQ_PATH="Alignment_*/*/Fastq/" ;;
         "${MISEQ_PATH}") local FASTQ_PATH="Data/Intensities/BaseCalls/" ;;
         "${NEXTSEQ_PATH}") local FASTQ_PATH="FastQs/" ;;
     esac
@@ -65,43 +68,45 @@ create_samplesheet () {
     while read -r SAMPLESHEET_LINE
         do
         local SAMPLE_NAME=${SAMPLESHEET_LINE%%,*}
-        local FASTQ1=$(ls ${SEQ_PATH}/${RUN}/${FASTQ_PATH} | grep "^${SAMPLE_NAME}_.*_R1_.*")
-        local FASTQ2=$(ls ${SEQ_PATH}/${RUN}/${FASTQ_PATH}/ | grep "^${SAMPLE_NAME}_.*_R2_.*")
+        local FASTQ1=$(realpath ${SEQ_PATH}/${RUN}/${FASTQ_PATH}/* | grep "^${SAMPLE_NAME}_.*_R1_.*")
+        local FASTQ2=$(realpath ${SEQ_PATH}/${RUN}/${FASTQ_PATH}/* | grep "^${SAMPLE_NAME}_.*_R2_.*")
         echo "${SAMPLE_NAME},${SAMPLE_NAME},1,${FASTQ1},${FASTQ2}" >>"${SEQ_PATH}${RUN}/${SAMPLESHEET_SAREK}"
     done
 }
 
 launch_sarek () { 
     # get genome version
-    VGENOME=$(echo "${BEDFILE}" | grep -oE 'hg[0-9]{2}')
+    VGENOME=$(echo "${BEDNAME}" | grep -oE 'hg[0-9]{2}')
     if [ "${VGENOME}" = "hg38" ]
     then 
+        BEDFILE="${BEDNAME}.bed"
         # string with genome-specific parameters
         PARAMS="--genome ${GENOME_38} \
-                --igenomes_base ${IGENOMES_BASE_38} \
-                --fasta ${FASTA_38} \
-                --fasta_fai ${FASTA_FAI_38}"
+        --igenomes_base ${IGENOMES_BASE_38} \
+        --fasta ${FASTA_38} \
+        --fasta_fai ${FASTA_FAI_38}"
     else
+        BEDFILE="${BEDNAME}_nochr.bed" #TODO: rename sarek beds
         # string with genome-specific parameters
         PARAMS="--genome ${GENOME_19} \
-                --igenomes_base ${IGENOMES_BASE_19} \
-                --fasta ${FASTA_19} \
-                --fasta_fai ${FASTA_FAI_19}"
+        --igenomes_base ${IGENOMES_BASE_19} \
+        --fasta ${FASTA_19} \
+        --fasta_fai ${FASTA_FAI_19}"
     fi
     COMMAND="nextflow run ${SAREK_PATH} -profile singularity \
-        -c ${CONFIG_PATH} \
-        -w "${SEQ_PATH}${RUN}/${WORKDIR}" \
-        --input "${SEQ_PATH}${RUN}/${SAMPLESHEET_SAREK}" \
-        --outdir "${SEQ_PATH}${RUN}/${OUTDIR_SAREK}" \
-        --step ${STEP} \
-        --tools ${TOOLS} \
-        --intervals ${BEDFILE} \
-        --wes ${WES} \
-        --save_output_as_bam ${SAVE_OUTPUT_AS_BAM} \
-        --concatenate_vcfs ${CONCATENATE_VCFS} \
-        --trim_fastq ${TRIM_FASTQ} \
-        --split_fastq ${SPLIT_FASTQ} \
-        ${PARAMS}"
+    -c ${CONFIG_PATH} \
+    -w "${SEQ_PATH}${RUN}/${WORKDIR}" \
+    --input "${SEQ_PATH}${RUN}/${SAMPLESHEET_SAREK}" \
+    --outdir "${SEQ_PATH}${RUN}/${OUTDIR_SAREK}" \
+    --step ${STEP} \
+    --tools ${TOOLS} \
+    --intervals ${BEDFILE} \
+    --wes ${WES} \
+    --save_output_as_bam ${SAVE_OUTPUT_AS_BAM} \
+    --concatenate_vcfs ${CONCATENATE_VCFS} \
+    --trim_fastq ${TRIM_FASTQ} \
+    --split_fastq ${SPLIT_FASTQ} \
+    ${PARAMS}"
     ${COMMAND} &> "${SEQ_PATH}${RUN}/${NF_STDOUT_FILE}"
 }
 
@@ -121,9 +126,7 @@ for SEQ_PATH in ${MINISEQ_PATH} ${MISEQ_PATH} ${NEXTSEQ_PATH}
 
     for RUN in ${RUN_LIST}
         do
-        set +e
         LINE=$(grep "${RUN}" "${SEQ_PATH}${RUNS_FILE}")
-        set -e
         # add the new run to runs file
         if [ -z "${LINE}" ]
         then
@@ -139,11 +142,11 @@ for SEQ_PATH in ${MINISEQ_PATH} ${MISEQ_PATH} ${NEXTSEQ_PATH}
             launch_sarek
             # get error
             FAIL_LINES=$(grep FAILED "${SEQ_PATH}${RUN}/${OUTDIR_SAREK}pipeline_info/execution_trace_*.txt" )
-            # change trace name
-            mv "${SEQ_PATH}${RUN}/${OUTDIR_SAREK}pipeline_info/execution_trace_*.txt" "${SEQ_PATH}${RUN}/${OUTDIR_SAREK}pipeline_info/_execution_trace_$(date +%F).txt"
             # if error is from custom dump software versions, continue, else, throw error
             if [[ $(echo "${FAIL_LINES}" | wc -l) -eq 1 ]] && [ -n "$(echo "${FAIL_LINES}" | grep DSV)" ]
             then
+                # change trace name
+                mv "${SEQ_PATH}${RUN}/${OUTDIR_SAREK}pipeline_info/execution_trace_*.txt" "${SEQ_PATH}${RUN}/${OUTDIR_SAREK}pipeline_info/_execution_trace_$(date +%F).txt"
                 # get temporary version file
                 VERSIONS_FILE=$(realpath "${SEQ_PATH}${RUN}/${WORKDIR}/tmp/*/*/collated_versions.yml")
                 VERSIONS_FILE_TMP=tmp_collated_versions
@@ -180,16 +183,14 @@ for SEQ_PATH in ${MINISEQ_PATH} ${MISEQ_PATH} ${NEXTSEQ_PATH}
                 then
                     # Pipeline completed ! change run status to done
                     change_status 2
+                    rm -r "${SEQ_PATH:?}${RUN:?}/${WORKDIR:?}"
                 else
-                    #TODO: throw error with run info?
                     change_status 3
                 fi
             else
-                #TODO: throw error with run info?
                 change_status 3
             fi
-            #TODO: remove temporary files (deal with other logs, may need CWD)
-            # rm -r "${SEQ_PATH:?}${RUN:?}/${WORKDIR}"
+            #TODO: remove logs
         fi
     done
 done
